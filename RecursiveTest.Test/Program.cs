510cd7364bb4f3a2e6170369dd889ce580743366
MusicDemons <pieterjandeclippel@hotmail.com>
2020-02-10 23:10:30 +0100

    Add project files.

---
+++ RecursiveTest.Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace RecursiveTest.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var context = new RecursiveContext();
+            var categories = context.Categories
+                .Include(cat => cat.Categories)
+                .AsEnumerable()
+                .Where(cat => cat.ParentCategory == null)
+                .ToList()
+                .Select(cat => ToDto(cat))
+                .ToList();
+
+            Debugger.Break();
+        }
+
+        static Dtos.Category ToDto(Entities.Category category)
+        {
+            return new Dtos.Category
+            {
+                Id = category.Id,
+                Description = category.Description,
+                Categories = category.Categories == null ? null : category.Categories.Select(c => ToDto(c)).ToList()
+            };
+        }
+    }
+}
---
+++ RecursiveTest/RecursiveContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RecursiveTest.Entities;
+using System;
+
+namespace RecursiveTest
+{
+    public class RecursiveContext : DbContext
+    {
+        public RecursiveContext()
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=Recursive;Trusted_Connection=True;ConnectRetryCount=0";
+            optionsBuilder.UseSqlServer(connectionString, options => options.MigrationsAssembly("RecursiveTest"));
+        }
+    }
+}
